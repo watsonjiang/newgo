e95a1a3258218f893d7baa3d600d748e54a0ba43
watsonjiang <watson.jiang@gmail.com>
2013-04-02 21:50:56 +0800

    remove pyc files.

--- static/__init__.py
+++ static/__init__.py
@@ -6,12 +6,12 @@ from webob import Request
 from webob import Response
 from webob.static import FileApp
 from watsonwebutil import WebMethod
+import os.path
 
-BASE_DIR="c:/watson/newgo"
+BASE_DIR='.'
 logger=logging.getLogger("newgo.static")
 
 def static_file_handler(file_path):
-    path = BASE_DIR + file_path
-    path = path.replace("/", "\\")
+    path = os.path.join(BASE_DIR, file_path[1:])
     logger.debug("Retrive static file %s", path)
     return FileApp(path); 
