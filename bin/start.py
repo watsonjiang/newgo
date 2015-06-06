3348531411c422d79860d915fbd5410ab535db8b
watson <watson.jiang@gmail.com>
2015-06-06 17:30:45 +0800

    init logging at start.py

--- bin/start.py
+++ bin/start.py
@@ -2,5 +2,14 @@
 # -*- coding:utf-8 -*-
 
 from newgo import run_server
+import logging
+import logging.config
+import os
+import os.path
+
+NEWGO_HOME = os.getenv('NEWGO_HOME')
+
+cf_name = os.path.join(NEWGO_HOME, 'conf/logging.conf')
+logging.config.fileConfig(cf_name)
 
 run_server()
